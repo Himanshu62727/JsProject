bd9eaf155984316f5336251245d6317354a39071
Himanshu Singh <124270542+Himanshu62727@users.noreply.github.com>
2024-10-13 11:26:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,36 +5,59 @@ let arr = [
     { id: 2, name: "jack", age: "20", profession: "developer" },
     { id: 3, name: "karen", age: "19", profession: "admin" },
   ];
-  
+
+
+//sol=>1
   function PrintDeveloperbyMap(arr) {
     //Write your code here , just console.log
-
-    let developers = arr.filter(person=>person.profession=="developer")
-    developers.map(developer=> {
-    console.log(developer)
-  })
-
+arr.map((arr) => {
+        if (arr.profession == "developer") {
+            console.log(arr);
+        }
+    });
   }
-  
+
+// sol=>2
   function PrintDeveloperbyForEach(arr) {
     //Write your code here , just console.log
-
-    let developers = arr.filter(person=>person.profession=="developer")
-    developers.forEach(developer=> {
-        console.log(developer)
-    })
+      arr.forEach((arr) => {
+        if (arr.profession == "developer") {
+            console.log(arr);
+        }
+    });
   }
-  
+
+//sol=>3
   function addData() {
     //Write your code here, just console.log
+   let newEmployee = { id: 4, name: "Nikhil", age: "23", profession: "Don" };
+       employees.push(newEmployee);
+        console.log(employees);
   }
-  
+
+
+//sol=>4
   function removeAdmin() {
     //Write your code here, just console.log
-    let filteredArr = arr.filter(employee=>employee.profession !="admin");
-    console.log(filteredArr);
+
+      let arr = arr.filter((arr) => {
+        if (arr.profession != "admin") {
+            console.log(arr)
+            }
+        });
   }
-  
+
+
+//sol=>5
   function concatenateArray() {
     //Write your code here, just console.log
+
+      let createNewEmployees = [
+        { id: 5, name: "Deepanshu", age: "22", profession: "Dancer" },
+        { id: 6, name: "Neeraj", age: "50", profession: "Fashion designer" },
+        { id: 7, name: "Himanshu", age: "22", profession: "CEO" }
+      ];
+      
+      let newEmployeeAdd = arr.concat(createNewEmployees);
+      console.log(newEmployeeAdd);
   }
